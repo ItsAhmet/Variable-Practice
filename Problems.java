dcc1b3ac707578f91e27df86740ee300efb56427
ItsAhmet <74107960+ItsAhmet@users.noreply.github.com>
2022-09-16 16:56:39 -0400

    Create Problems.java

---
+++ Problems.java
@@ -0,0 +1,59 @@
+//Name: Ahmet Yesilbas
+//Project Name: Problems
+//Date: 10/1/2021
+//Project Description: Solving problems using mainly variables.
+
+public class Problems {
+	public static void main(String args[]) {
+		
+		
+		//Problem #1
+		int male=8;
+		int female=12;
+
+		System.out.println("Percentage of males = "+ male+"/20 = %"+(8.00/10)*(50));
+		System.out.println("Percentage of females = "+ female+"/20 = %"+(12.00/10)*(50));
+		
+		System.out.println();
+		//Problem #2
+	
+		
+		
+		int James=85;
+		int Jack=78;
+		int Joe=96;
+		
+		
+		String Student1="James";
+		String Student2="Jack";
+		String Student3="Joe";
+		
+		
+		System.out.println(Student1+" got an "+James);
+		System.out.println(Student2+" got an "+Jack);
+		System.out.println(Student3+" got an "+Joe);
+		System.out.println("\nThe class average on the test was "+(James+Jack+Joe)/3);
+		
+		System.out.println();
+		//Problem #3
+		
+		
+		
+		double EmpOneH=39.35;
+		double EmpOneS=8.66;
+		
+		System.out.println("Employee #1");
+		System.out.println("Hours worked= "+EmpOneH);
+		System.out.println("Hourly pay rate= "+EmpOneS);
+		System.out.println("Gross Pay= "+EmpOneH*EmpOneS);
+		
+		double EmpTwoH=39.35;
+		double EmpTwoS=8.66;
+		
+		System.out.println("Employee #2");
+		System.out.println("Hours worked= "+EmpTwoH);
+		System.out.println("Hourly pay rate= "+EmpTwoS);
+		System.out.println("Gross Pay= "+EmpTwoH*EmpTwoS);
+		
+	}
+}
